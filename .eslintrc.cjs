6f8c4b1ada05642bbdbecb510b3b3945eb615ed5
Cheder-one <cherbadenis1@gmail.com>
2023-06-09 15:49:18 +0300

    u

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2022: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended"
+    "plugin:react-hooks/recommended",
+    "standard"
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "react"],
   rules: {
     "react-refresh/only-export-components": "warn",
     "multiline-ternary": "off",
