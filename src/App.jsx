c8c6693ef6c8f2e6a53dbe94bd7292e9aa940c73
Cheder-one <cherbadenis1@gmail.com>
2023-06-09 19:41:59 +0300

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>App</h1>
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
 }
 
 export default App;
