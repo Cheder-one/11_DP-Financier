47971600058ac00e5f02f7fc6a65b8d45667c80f
Cheder-one <cherbadenis1@gmail.com>
2023-06-20 16:35:17 +0300

    u

--- src/app/api/fake.api/users/users.api.js
+++ src/app/api/fake.api/users/users.api.js
@@ -1,14 +1,13 @@
 import { accounts } from "";
-import { qualities } from "./qualities.api";
 
-const users = [
+export const users = [
   {
-    _id: "67rdca3eeb7f6fgeed471815",
+    _id: "user-id-1",
     name: "Джон Дориан",
     email: "Jony7351@tw.com",
     password: "jony7351",
     gender: "male",
-    accounts: professions.doctor,
+    accounts: accounts.doctor,
     qualities: [qualities.tedious, qualities.uncertain, qualities.strange],
     completedMeetings: 36,
     rate: 2.5,
