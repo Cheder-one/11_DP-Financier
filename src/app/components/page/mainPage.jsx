75fa79e7d16b8e003a11c6d27d631de6fb9a1b5a
Cheder-one <cherbadenis1@gmail.com>
2023-07-09 22:06:13 +0300

    Обнаружен баг. При смене счета -> Выбор даты в карточке -> Выбор "Все" счета - в транзакциях будут все транзакции прошлого счета. После повторного нажатия на "Все" выведутся транзакции всех счетов

--- src/app/components/page/mainPage.jsx
+++ src/app/components/page/mainPage.jsx
@@ -2,13 +2,20 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import { Button, Col, Row } from "react-bootstrap";
 import { filter, find, keys, uniqBy } from "lodash";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { BiSolidPlusSquare as PlusSquare } from "react-icons/bi";
 import { LiaWindowCloseSolid as CloseX } from "react-icons/lia";
 import { toReadableDate } from "../../utils/functions/toReadableDate";
 import Loader from "../ui/spinner";
 import AccountCard from "../common/card/accountCard";
 import Dropdown from "../common/form/dropdown";
+
+const getUniqDates = (data) => {
+  return uniqBy(data, "date").map((uniq) => ({
+    ...uniq,
+    name: toReadableDate(uniq.date).dateOnly
+  }));
+};
 
 const MainPage = ({ userId }) => {
   const [user, setUser] = useState({});
@@ -44,31 +51,17 @@ const MainPage = ({ userId }) => {
     fetchData();
   }, [userId]);
 
-  const getCategoryName = (item) => {
-    return find(categories, { id: item.category }).name;
-  };
-
   // Трансформирую данные body карточки, чтобы универсальный компонент мог принимать любые значения, независимо от их name.
-  const getCardBodyColumnItems = useCallback(
-    (cards) => {
-      keys(cards).forEach((key) => {
-        const card = cards[key];
-        card.forEach((item) => {
-          item.firstCol = item.amount;
-          item.secondCol = getCategoryName(item);
-          item.thirdCol = null;
-        });
+  const getCardBodyColumnItems = (cards) => {
+    keys(cards).forEach((key) => {
+      const card = cards[key];
+      card.forEach((item) => {
+        item.firstCol = item.amount;
+        item.secondCol = find(categories, { id: item.category }).name;
+        item.thirdCol = null;
       });
-      return cards;
-    },
-    [cardBodyItems]
-  );
-
-  const getUniqDates = (data) => {
-    return uniqBy(data, "date").map((uniq) => ({
-      ...uniq,
-      name: toReadableDate(uniq.date).dateOnly
-    }));
+    });
+    return cards;
   };
 
   const filteredByUniqAndType = useMemo(() => {
@@ -140,107 +133,106 @@ const MainPage = ({ userId }) => {
     }
   };
 
-  useMemo(() => getCardBodyColumnItems(cardBodyItems), [cardBodyItems]);
+  getCardBodyColumnItems(cardBodyItems);
 
-  const dropDownIncome = (
-    <Dropdown
-      items={income.uniqDates}
-      type="income"
-      onSelect={handleDropdownSelect}
-      reset={selectedAccount.reset}
-    />
-  );
+  if (keys(user || {}).length > 0) {
+    const dropDownIncome = (
+      <Dropdown
+        items={income.uniqDates}
+        type="income"
+        onSelect={handleDropdownSelect}
+        reset={selectedAccount.reset}
+      />
+    );
+    const dropDownAccount = (
+      <Dropdown
+        items={accounts}
+        type="account"
+        onSelect={handleDropdownSelect}
+      />
+    );
+    const dropDownExpense = (
+      <Dropdown
+        items={expense.uniqDates}
+        type="expense"
+        onSelect={handleDropdownSelect}
+        reset={selectedAccount.reset}
+      />
+    );
 
-  const dropDownAccount = (
-    <Dropdown items={accounts} type="account" onSelect={handleDropdownSelect} />
-  );
+    const addButton = (
+      <Button variant="" className="p-0">
+        <PlusSquare style={{ color: "yellowgreen" }} size={25} />
+      </Button>
+    );
+    const delButton = (
+      <Button variant="" size="sm" className="p-0">
+        <CloseX style={{ color: "red" }} size={19} />
+      </Button>
+    );
 
-  const dropDownExpense = (
-    <Dropdown
-      items={expense.uniqDates}
-      type="expense"
-      onSelect={handleDropdownSelect}
-      reset={selectedAccount.reset}
-    />
-  );
+    return (
+      <div className="mx-4">
+        <Row style={{ marginTop: "3%" }}>
+          <Col md="4">
+            <AccountCard
+              title={{
+                first: "Доход",
+                second: dropDownIncome,
+                third: addButton
+              }}
+              route="/"
+              bodyList={cardBodyItems.income}
+              bodyCol={{
+                third: delButton
+              }}
+              dropDownList={income.uniqDates}
+            />
+          </Col>
+          <Col md="4">
+            <AccountCard
+              title={{
+                first: "Счет",
+                second: dropDownAccount,
+                third: addButton
+              }}
+              route="/"
+              bodyList={cardBodyItems.account}
+              bodyCol={{
+                third: delButton
+              }}
+              dropDownList={accounts}
+            />
+          </Col>
+          <Col md="4">
+            <AccountCard
+              title={{
+                first: "Расход",
+                second: dropDownExpense,
+                third: addButton
+              }}
+              route="/"
+              bodyList={cardBodyItems.expense}
+              bodyCol={{
+                third: delButton
+              }}
+              dropDownList={expense.uniqDates}
+            />
+          </Col>
+        </Row>
 
-  const addButton = (
-    <Button variant="" className="p-0">
-      <PlusSquare style={{ color: "yellowgreen" }} size={25} />
-    </Button>
-  );
-
-  const delButton = (
-    <Button variant="" size="sm" className="p-0">
-      <CloseX style={{ color: "red" }} size={19} />
-    </Button>
-  );
-
-  return (
-    <>
-      {keys(user || {}).length > 0 ? (
-        <div className="mx-4">
-          <Row style={{ marginTop: "3%" }}>
-            <Col md="4">
-              <AccountCard
-                title={{
-                  first: "Доход",
-                  second: dropDownIncome,
-                  third: addButton
-                }}
-                route="/"
-                bodyList={cardBodyItems.income}
-                bodyCol={{
-                  third: delButton
-                }}
-                dropDownList={income.uniqDates}
-              />
-            </Col>
-            <Col md="4">
-              <AccountCard
-                title={{
-                  first: "Счет",
-                  second: dropDownAccount,
-                  third: addButton
-                }}
-                route="/"
-                bodyList={cardBodyItems.account}
-                bodyCol={{
-                  third: delButton
-                }}
-                dropDownList={accounts}
-              />
-            </Col>
-            <Col md="4">
-              <AccountCard
-                title={{
-                  first: "Расход",
-                  second: dropDownExpense,
-                  third: addButton
-                }}
-                route="/"
-                bodyList={cardBodyItems.expense}
-                bodyCol={{
-                  third: delButton
-                }}
-                dropDownList={expense.uniqDates}
-              />
-            </Col>
-          </Row>
-
-          <Row style={{ marginTop: "3%" }}>
-            <Col>
-              <div className="flex justify-center items-center border border-dark vh-40">
-                <h5>Element</h5>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      ) : (
-        <Loader className="flex justify-center items-center vh-30" />
-      )}
-    </>
-  );
+        <Row style={{ marginTop: "3%" }}>
+          <Col>
+            <div className="flex justify-center items-center border border-dark vh-40">
+              <h5>Element</h5>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  } else {
+    return <Loader className="flex justify-center items-center vh-30" />;
+  }
 };
 
 MainPage.propTypes = {
