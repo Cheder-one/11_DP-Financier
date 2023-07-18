a438aa35bb6096bdc6267357b785da174c6be512
Cheder-one <cherbadenis1@gmail.com>
2023-07-18 15:12:11 +0300

    Системный баг с EmptyIcon исправлен обновлением значений

--- src/app/assets/icons/emptyIcon.jsx
+++ src/app/assets/icons/emptyIcon.jsx
@@ -1,10 +1,10 @@
-const EmptyIcon = ({ height, width }) => {
+const EmptyIcon = () => {
   return (
     <div className="cursor-pointer">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={width}
-        height={height}
+        width={"20px"}
+        height={"20px"}
         // color={color}
       >
         {/* {color ? <rect width="100%" height="100%" fill={color} /> : ""} */}
