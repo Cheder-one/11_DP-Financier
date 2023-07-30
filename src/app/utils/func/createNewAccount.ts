61c322b4590a5477bd1abdf98a72f8af5dd37852
Cheder-one <cherbadenis1@gmail.com>
2023-07-31 01:21:05 +0300

    Добавляю на сервер недостающие значения accountTypes и тд

--- src/app/utils/func/createNewAccount.ts
+++ src/app/utils/func/createNewAccount.ts
@@ -1,40 +1,33 @@
 import { Account, Category } from "../../types/userTypes";
 import getAmountByType from "./getAmountByType";
 
-
-// account,
-// currency,
-// name,
-// icon,
-// iconColor,
-// balance,
-// comment
-const createNewAccount = ({
-  newAccountId,
-      account: {},
-      currency: {},
-      name: "",
-      icon: "VscBlank",
-      iconColor: "#00000",
-      balance: "",
-      comment: ""
-}: {}) => {
-  const newAccount = {
-       id: "account-id-1",
-       type: "account",
-       name: name.trim(),
-       public: false,
-       category: "category-id-1",
-       balance: 90000,
-       transactions: [
-        "transaction-id-1",
-        "transaction-id-2",
-        "transaction-id-5",
-        "transaction-id-6"
-      ],
-      comment: ""
+const createNewTransact = ({
+  newTransactId,
+  amount,
+  cardType,
+  account,
+  category,
+  date,
+  comment
+}: {
+  newTransactId: string;
+  amount: string;
+  cardType: string;
+  account: Account;
+  category: Category;
+  date: Date;
+  comment: string | "";
+}) => {
+  const newTransaction = {
+    id: newTransactId,
+    amount: getAmountByType(amount, cardType),
+    type: cardType,
+    account: account.id,
+    category: category.id,
+    date: date?.toISOString(),
+    comment
   };
-  return newAccount;
+  return newTransaction;
 };
 
-export default createNewAccount;
+export default createNewTransact;
