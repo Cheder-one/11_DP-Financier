2798efb0e08e223923dead2942366c351aaf980c
Cheder-one <cherbadenis1@gmail.com>
2023-08-14 05:58:00 +0300

    Пора начинать адаптировать данные сервера

--- src/app/components/ui/chartTabs/incomeTab.jsx
+++ src/app/components/ui/chartTabs/incomeTab.jsx
@@ -1,5 +1,6 @@
 import { BarChart, axisClasses } from "@mui/x-charts";
 import { useWindowInnerWidth } from "../../../utils";
+import { Box, Slider } from "@mui/material";
 import { useState } from "react";
 
 // Генерация случайного числа в заданном диапазоне
@@ -23,22 +24,22 @@ const generateData = () => {
   return data;
 };
 
+const categoryColor = null;
+
+const getScrollClass = (windowWidth) => {
+  return windowWidth > 800 ? "" : "overflow-scroll";
+};
+const valueFormatter = (value) => {
+  return `${value} $`;
+};
+const getColor = () => {
+  return categoryColor ? { color: categoryColor } : {};
+};
+
 const data = generateData();
 
 const BarsDataset = () => {
   const [windowWidth] = useWindowInnerWidth();
-
-  const categoryColor = null;
-
-  const getScrollClass = () => {
-    return windowWidth > 800 ? "" : "overflow-scroll";
-  };
-  const valueFormatter = (value) => {
-    return `${value} $`;
-  };
-  const getColor = () => {
-    return categoryColor ? { color: categoryColor } : {};
-  };
 
   const colorScheme = getColor();
 
@@ -76,45 +77,82 @@ const BarsDataset = () => {
       }
     ],
     height: 250,
-    width: windowWidth > 800 ? windowWidth - 32 : 800
+    width: windowWidth > 800 ? windowWidth - 32 : 800,
+    margin: { top: 10, bottom: 60, left: 40, right: 30 }
+  };
+
+  const minDistance = 1;
+
+  const [value, setValue] = useState([10, 20]);
+
+  const handleChange = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 100 - minDistance);
+        setValue([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue([clamped - minDistance, clamped]);
+      }
+    } else {
+      setValue(newValue);
+    }
   };
 
   return (
-    <div className={getScrollClass()}>
-      <BarChart
-        dataset={data}
-        {...chartParams}
-        xAxis={[
-          {
-            dataKey: "day",
-            scaleType: "band",
-            barGapRatio: 0.2,
-            categoryGapRatio: 0.3
-          }
-        ]}
-        series={chartParams.series.map((series) => ({
-          ...series,
-          highlightScope: {
-            highlighted: "series",
-            faded: "global"
-          }
-        }))}
-        legend={{
-          direction: "row",
-          position: {
-            vertical: "bottom",
-            horizontal: "middle"
-          }
-        }}
-        sx={{
-          [`.${axisClasses.left} .${axisClasses.label}`]: {
-            transform: "rotate(-70deg) "
-          },
-          "--ChartsLegend-rootOffsetX": "0px",
-          "--ChartsLegend-rootOffsetY": "30px"
-        }}
-        margin={{ top: 10, bottom: 60, left: 40, right: 30 }}
-      />
+    <div className={getScrollClass(windowWidth)}>
+      <Box sx={{ width: "100%", maxWidth: "50%" }}>
+        <BarChart
+          dataset={data}
+          {...chartParams}
+          xAxis={[
+            {
+              label: "dsd",
+              dataKey: "day",
+              scaleType: "band",
+              barGapRatio: 0.2,
+              categoryGapRatio: 0.3,
+              min: value[0],
+              max: value[1]
+            }
+          ]}
+          series={chartParams.series.map((series) => ({
+            ...series,
+            highlightScope: {
+              highlighted: "series",
+              faded: "global"
+            }
+          }))}
+          legend={{
+            direction: "row",
+            position: {
+              vertical: "bottom",
+              horizontal: "middle"
+            }
+          }}
+          sx={{
+            // [`.${axisClasses.bottom} .${axisClasses.label}`]: {
+            //   transform: "rotate(-50deg)",
+            //   transformOrigin: "center"
+            // },
+            "--ChartsLegend-rootOffsetX": "0px",
+            "--ChartsLegend-rootOffsetY": "30px"
+          }}
+        />
+        {/* <Slider
+          className="ml-50%"
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          min={1}
+          max={30}
+          sx={{ mt: 2 }}
+        /> */}
+      </Box>
     </div>
   );
 };
